647c97982f11de1b5dc5680789964b896109dda6
Ruediger Klaehn <rklaehn@protonmail.com>
2023-11-21 17:44:17 +0100

    WIP

--- directory-watcher/src/main.rs
+++ directory-watcher/src/main.rs
@@ -7,17 +7,47 @@ mod io;
 use args::{Args, WatchArgs};
 use bytes::Bytes;
 use io::setup_logging;
-use iroh::{
-    client::{Doc, Iroh},
-    rpc_protocol::{ProviderRequest, ProviderResponse},
-    sync::AuthorId,
-};
 use notify::{event::CreateKind, Event, EventKind, RecursiveMode, Watcher};
 
-fn get_doc_and_key<C>(client: &Iroh<C>, path: &Path) -> Option<(Doc<C>, AuthorId, Bytes)>
-where
-    C: quic_rpc::transport::Connection<ProviderResponse, ProviderRequest>,
-{
+/// Is this file relevant for insertion?
+fn is_relevant(path: &Path) -> bool {
+    let text = path.to_string_lossy();
+    text.starts_with("Maildir/new/") || text.starts_with("Maildir/cur/")
+}
+
+/// Get a key given an email.
+fn get_key(data: &[u8]) -> Option<String> {
+    let headers = utf8_prefix(data);
+    let message_id = extract_message_id(headers)?;
+    let key = format!("/inbox/{}", message_id);
+    Some(key)
+}
+
+/// Get the UTF-8 prefix of a blob. This should parse all ascii headers
+fn utf8_prefix(blob: &[u8]) -> &str {
+    match std::str::from_utf8(blob) {
+        Ok(s) => s, // If the entire blob is valid UTF-8, return it.
+        Err(e) => {
+            // If there's an error, get the index of the first invalid byte.
+            let valid_up_to = e.valid_up_to();
+            // Convert the valid part of the blob to a str safely.
+            std::str::from_utf8(&blob[..valid_up_to]).unwrap_or_default()
+        }
+    }
+}
+
+fn extract_message_id(email: &str) -> Option<String> {
+    for line in email.lines() {
+        if line.is_empty() {
+            return None;
+        }
+        if line.to_lowercase().starts_with("message-id:") {
+            let parts: Vec<&str> = line.splitn(2, ':').collect();
+            if parts.len() == 2 {
+                return Some(parts[1].trim().to_string());
+            }
+        }
+    }
     None
 }
 
@@ -50,37 +80,47 @@ async fn watch_cmd(args: WatchArgs) -> anyhow::Result<()> {
         .await?;
     let iroh = node.client();
     let doc = iroh.docs.create().await?;
+    println!("Writing to document {}", doc.id());
     let author = iroh.authors.create().await?;
+    println!("Using author {}", author);
 
     // Automatically select the best implementation for your platform.
     let root = args.path.canonicalize()?;
     let root2 = root.clone();
-    let mut watcher = notify::recommended_watcher(move |res| match res {
-        Ok(Event { kind, paths, attrs }) => match kind {
-            EventKind::Create(CreateKind::File) => {
-                for path in paths {
-                    tracing::error!("create: {:?} {:?}", path, root2);
-                    if let Some(rel) = make_relative(&root2, &path) {
-                        tracing::error!("create: {:?}", rel);
-                        if let Some((doc, author, key)) = get_doc_and_key(&iroh, &rel) {
-                            tokio::spawn(async move {
-                                let t = doc.import_file(author, key, path, true).await?;
-                                let outcome = t.finish().await?;
-                                tracing::error!("create: {:?} {:?}", rel, outcome);
-                                anyhow::Ok(())
-                            });
-                        }
-                    }
-                    // let progress = doc.import_file(author, key, path, true).await?;
-                }
-            }
-            other => {
-                tracing::info!("other: {:?}", other);
-            }
-        },
-        Err(e) => tracing::error!("watch error: {:?}", e),
+    let (send, mut recv) = tokio::sync::mpsc::unbounded_channel();
+    let mut watcher = notify::recommended_watcher(move |msg| {
+        send.send(msg).ok();
     })?;
     watcher.watch(&root, RecursiveMode::Recursive)?;
+    tokio::spawn(async move {
+        while let Some(msg) = recv.recv().await {
+            let Ok(event) = msg else {
+                continue;
+            };
+            let EventKind::Create(CreateKind::File) = event.kind else {
+                continue;
+            };
+            for path in event.paths {
+                tracing::error!("create: {:?} {:?}", path, root2);
+                if let Some(relative_path) = make_relative(&root2, &path) {
+                    tracing::error!("create: {:?}", relative_path);
+                    if is_relevant(&relative_path) {
+                        if let Ok(bytes) = std::fs::read(&path) {
+                            if let Some(key) = get_key(&bytes) {
+                                println!("Found something that looks like a mail");
+                                println!("Using key {}", key);
+                                let key = Bytes::from(key);
+                                let doc = doc.clone();
+                                let hash = doc.set_bytes(author, key, bytes).await?;
+                                println!("added entry with hash {}", hash);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        anyhow::Ok(())
+    });
     std::io::stdin().read_line(&mut String::new())?;
     Ok(())
 }
