27bca769b5720220f0140f72e9c21429a4508845
Ruediger Klaehn <rklaehn@protonmail.com>
2023-11-21 15:23:36 +0100

    more stuff

--- directory-watcher/src/main.rs
+++ directory-watcher/src/main.rs
@@ -1,10 +1,41 @@
+use std::path::{Path, PathBuf};
+
 use clap::Parser;
 mod args;
 mod config;
 mod io;
 use args::{Args, WatchArgs};
+use bytes::Bytes;
 use io::setup_logging;
+use iroh::{
+    client::{Doc, Iroh},
+    rpc_protocol::{ProviderRequest, ProviderResponse},
+    sync::AuthorId,
+};
 use notify::{event::CreateKind, Event, EventKind, RecursiveMode, Watcher};
+
+fn get_doc_and_key<C>(client: &Iroh<C>, path: &Path) -> Option<(Doc<C>, AuthorId, Bytes)>
+where
+    C: quic_rpc::transport::Connection<ProviderResponse, ProviderRequest>,
+{
+    None
+}
+
+fn make_relative<'a>(base: &'a Path, target: &'a Path) -> Option<PathBuf> {
+    let base = base.components().collect::<Vec<_>>();
+    let mut target = target.components().collect::<Vec<_>>();
+    let n = base.len();
+    if target.len() < base.len() {
+        return None;
+    }
+    for i in 0..n {
+        if base[i] != target[i] {
+            return None;
+        }
+    }
+    target.splice(..n, []);
+    Some(target.iter().collect())
+}
 
 async fn watch_cmd(args: WatchArgs) -> anyhow::Result<()> {
     let blobs_path = std::env::current_dir()?.join("blobs");
@@ -13,18 +44,34 @@ async fn watch_cmd(args: WatchArgs) -> anyhow::Result<()> {
     let blob_store =
         iroh::bytes::store::flat::Store::load_blocking(&blobs_path, &blobs_path, &blobs_path, &rt)?;
     let doc_store = iroh::sync::store::fs::Store::new(docs_path)?;
-    let iroh = iroh::node::Node::builder(blob_store, doc_store)
+    let node = iroh::node::Node::builder(blob_store, doc_store)
         .runtime(&rt)
         .spawn()
         .await?;
-    let client = iroh.client();
+    let iroh = node.client();
+    let doc = iroh.docs.create().await?;
+    let author = iroh.authors.create().await?;
 
     // Automatically select the best implementation for your platform.
-    let mut watcher = notify::recommended_watcher(|res| match res {
+    let root = args.path.canonicalize()?;
+    let root2 = root.clone();
+    let mut watcher = notify::recommended_watcher(move |res| match res {
         Ok(Event { kind, paths, attrs }) => match kind {
             EventKind::Create(CreateKind::File) => {
                 for path in paths {
-                    tracing::info!("create: {:?}", path);
+                    tracing::error!("create: {:?} {:?}", path, root2);
+                    if let Some(rel) = make_relative(&root2, &path) {
+                        tracing::error!("create: {:?}", rel);
+                        if let Some((doc, author, key)) = get_doc_and_key(&iroh, &rel) {
+                            tokio::spawn(async move {
+                                let t = doc.import_file(author, key, path, true).await?;
+                                let outcome = t.finish().await?;
+                                tracing::error!("create: {:?} {:?}", rel, outcome);
+                                anyhow::Ok(())
+                            });
+                        }
+                    }
+                    // let progress = doc.import_file(author, key, path, true).await?;
                 }
             }
             other => {
@@ -33,7 +80,7 @@ async fn watch_cmd(args: WatchArgs) -> anyhow::Result<()> {
         },
         Err(e) => tracing::error!("watch error: {:?}", e),
     })?;
-    watcher.watch(&args.path, RecursiveMode::Recursive)?;
+    watcher.watch(&root, RecursiveMode::Recursive)?;
     std::io::stdin().read_line(&mut String::new())?;
     Ok(())
 }
