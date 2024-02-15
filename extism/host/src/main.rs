6bb130b84a6f2c5a3bd8e9d65550f7672965160c
dignifiedquire <me@dignifiedquire.com>
2024-02-15 09:18:50 +0100

    pass ticket from the cli

--- extism/host/src/main.rs
+++ extism/host/src/main.rs
@@ -8,6 +8,8 @@ fn main() -> anyhow::Result<()> {
         .build()?;
     let rt = tokio_rt.handle().clone();
 
+    let ticket = std::env::args().nth(1).expect("missing ticket");
+
     let iroh = rt.block_on(async {
         let iroh_path = iroh_extism_host_functions::default_iroh_extism_data_root().await?;
         iroh_extism_host_functions::create_iroh(iroh_path).await
@@ -23,10 +25,10 @@ fn main() -> anyhow::Result<()> {
         iroh_extism_host_functions::add_all_host_functions(rt, plugin, iroh).build()?;
 
     let res = plugin
-        .call::<&str, &str>("print_hai_and_get_ticket", "blobaaa54kekl2oa7yd5ro3u65kynwolq3h2msu65c3jfj44ja23fstmcajcnb2hi4dthixs65ltmuys2mjomrsxe4bonfzg62bonzsxi53pojvs4lydabefsibbyrlqbqfiirdmivybeyaeaqktotuaaaaaaaaaaaaqb7cvoah75ql4x2hvxaxxlgje2d7qy2avizyjb25pd4anc6c5ulvgpth5xq")
+        .call::<&str, &str>("print_hai_and_get_ticket", &ticket)
         .unwrap();
 
-    println!("{}", res);
+    println!("Received iroh data:\n\n{}", res);
 
     Ok(())
 }
