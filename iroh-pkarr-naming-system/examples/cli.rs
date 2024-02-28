d03880db6976ef924967e310c4c3d5fb0b64d0b2
Ruediger Klaehn <rklaehn@protonmail.com>
2024-02-28 17:47:35 +0200

    Implement parsing of tickets, hashes or haf for content

--- iroh-pkarr-naming-system/examples/cli.rs
+++ iroh-pkarr-naming-system/examples/cli.rs
@@ -1,6 +1,55 @@
-use iroh_bytes::HashAndFormat;
+use iroh_bytes::{Hash, HashAndFormat};
+use iroh_net::ticket::BlobTicket;
 use iroh_pkarr_naming_system::{Record, IPNS};
-use std::{process, str::FromStr};
+use std::{fmt::Display, process, str::FromStr};
+
+/// Various ways to specify content.
+#[derive(Debug, Clone, derive_more::From)]
+pub enum ContentArg {
+    Hash(Hash),
+    HashAndFormat(HashAndFormat),
+    Ticket(BlobTicket),
+}
+
+impl ContentArg {
+    /// Get the hash and format of the content.
+    pub fn hash_and_format(&self) -> HashAndFormat {
+        match self {
+            ContentArg::Hash(hash) => HashAndFormat::raw(*hash),
+            ContentArg::HashAndFormat(haf) => *haf,
+            ContentArg::Ticket(ticket) => HashAndFormat {
+                hash: ticket.hash(),
+                format: ticket.format(),
+            },
+        }
+    }
+}
+
+impl Display for ContentArg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ContentArg::Hash(hash) => Display::fmt(hash, f),
+            ContentArg::HashAndFormat(haf) => Display::fmt(haf, f),
+            ContentArg::Ticket(ticket) => Display::fmt(ticket, f),
+        }
+    }
+}
+
+impl FromStr for ContentArg {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(hash) = Hash::from_str(s) {
+            Ok(hash.into())
+        } else if let Ok(haf) = HashAndFormat::from_str(s) {
+            Ok(haf.into())
+        } else if let Ok(ticket) = BlobTicket::from_str(s) {
+            Ok(ticket.into())
+        } else {
+            anyhow::bail!("invalid hash and format")
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -24,7 +73,7 @@ async fn main() -> anyhow::Result<()> {
             let secret_key = iroh_net::key::SecretKey::from_str(&args[0])?;
             let public_key = secret_key.public();
             let zid = pkarr::PublicKey::try_from(*public_key.as_bytes())?.to_z32();
-            let content = HashAndFormat::from_str(&args[1])?;
+            let content = ContentArg::from_str(&args[1])?.hash_and_format();
             let record = Record::Content { content };
             let ipns = IPNS::default();
             println!("Publishing record to: {}", public_key);
