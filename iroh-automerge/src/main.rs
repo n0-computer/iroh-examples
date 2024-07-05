d3ff0252c0685c333f14db640e789b409011ceaa
Philipp Krüger <philipp.krueger1@gmail.com>
2024-07-05 13:32:00 +0200

    refactor(iroh-automerge): Make iroh-automerge based on iroh, instead of iroh-net

--- iroh-automerge/src/main.rs
+++ iroh-automerge/src/main.rs
@@ -1,93 +1,173 @@
-use std::ops::{Deref, DerefMut};
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+};
 
-use anyhow::{ensure, Result};
+use anyhow::Result;
 use automerge::{
     sync::{self, SyncDoc},
     transaction::Transactable,
     AutoCommit, ReadDoc,
 };
 use clap::Parser;
-use iroh_net as inet;
+use iroh::node::{Node, ProtocolHandler};
 use serde::{Deserialize, Serialize};
 
-struct Peer {
-    auto_commit: AutoCommit,
-    sync_state: sync::State,
-    storage: Vec<u8>,
-    ep: inet::Endpoint,
+#[derive(Debug)]
+struct IrohAutomergeProtocol {
+    inner: Mutex<AutomergeState>,
 }
 
-const ALPN: &[u8] = b"iroh/automerge/1";
-const DEFAULT_PORT: u16 = 8888;
+#[derive(Debug)]
+struct AutomergeState {
+    commit: AutoCommit,
+}
 
-impl Peer {
-    async fn new() -> Result<Self> {
-        let ep = inet::Endpoint::builder()
-            .alpns(vec![ALPN.to_vec()])
-            .bind(DEFAULT_PORT)
-            .await?;
+impl IrohAutomergeProtocol {
+    const ALPN: &'static [u8] = b"iroh/automerge/1";
 
-        let addr = ep.node_addr().await?;
-        println!(
-            "Running \nNode Id: {}\n{}",
-            addr.node_id,
-            addr.info.relay_url.unwrap()
-        );
-
-        Ok(Self {
-            auto_commit: AutoCommit::new(),
-            sync_state: sync::State::new(),
-            storage: Vec::new(),
-            ep,
+    pub fn new(commit: AutoCommit) -> Arc<Self> {
+        Arc::new(Self {
+            inner: Mutex::new(AutomergeState { commit }),
         })
     }
 
-    fn save(&mut self) {
-        self.storage = self.auto_commit.save();
+    pub fn fork_state(&self) -> AutoCommit {
+        let mut guard = self.inner.lock().expect("lock poisoned");
+        guard.commit.fork()
     }
 
-    #[allow(unused)]
-    async fn load(data: &[u8]) -> Result<Self> {
-        let ep = inet::Endpoint::builder()
-            .alpns(vec![ALPN.to_vec()])
-            .bind(DEFAULT_PORT)
-            .await?;
-
-        let auto_commit = AutoCommit::load(data)?;
-        Ok(Self {
-            auto_commit,
-            sync_state: sync::State::new(),
-            storage: data.to_vec(),
-            ep,
-        })
+    pub fn merge_state(&self, mut commit: AutoCommit) {
+        let mut guard = self.inner.lock().expect("lock poisoned");
+        guard.commit.merge(&mut commit);
     }
 
-    fn generate_sync_message(&mut self) -> Option<sync::Message> {
-        self.auto_commit
-            .sync()
-            .generate_sync_message(&mut self.sync_state)
+    pub async fn initiate_sync(
+        self: Arc<Self>,
+        conn: iroh::net::endpoint::Connection,
+    ) -> Result<()> {
+        let (mut send, mut recv) = conn.open_bi().await?;
+
+        let mut commit = self.fork_state();
+        let mut sync = commit.sync();
+        let mut sync_state = sync::State::new();
+
+        let mut is_local_done = false;
+        loop {
+            let msg = match sync.generate_sync_message(&mut sync_state) {
+                Some(msg) => Protocol::SyncMessage(msg.encode()),
+                None => Protocol::Done,
+            };
+
+            if !is_local_done {
+                let encoded = postcard::to_stdvec(&msg)?;
+                send.write_all(&(encoded.len() as u64).to_le_bytes())
+                    .await?;
+                send.write_all(&encoded).await?;
+                is_local_done = matches!(msg, Protocol::Done);
+            }
+
+            let mut incoming_len = [0u8; 8];
+            recv.read_exact(&mut incoming_len).await?;
+            let len = u64::from_le_bytes(incoming_len);
+
+            let mut buffer = vec![0u8; len as usize];
+            recv.read_exact(&mut buffer).await?;
+            let msg: Protocol = postcard::from_bytes(&buffer)?;
+
+            let is_remote_done = matches!(msg, Protocol::Done);
+
+            // process incominng message
+            if let Protocol::SyncMessage(sync_msg) = msg {
+                let sync_msg = sync::Message::decode(&sync_msg)?;
+                sync.receive_sync_message(&mut sync_state, sync_msg)?;
+            }
+
+            if is_remote_done && is_local_done {
+                // both sides are done
+                break;
+            }
+        }
+
+        send.finish().await?;
+
+        drop(sync);
+        self.merge_state(commit);
+
+        Ok(())
     }
 
-    fn receive_sync_message(&mut self, msg: sync::Message) -> Result<()> {
-        self.auto_commit
-            .sync()
-            .receive_sync_message(&mut self.sync_state, msg)?;
+    pub async fn respond_sync(
+        self: Arc<Self>,
+        conn: iroh::net::endpoint::Connecting,
+    ) -> Result<()> {
+        let (mut send, mut recv) = conn.await?.accept_bi().await?;
 
-        self.save();
+        let mut commit = self.fork_state();
+        let mut sync = commit.sync();
+        let mut sync_state = sync::State::new();
+
+        let mut is_local_done = false;
+        loop {
+            let mut incoming_len = [0u8; 8];
+            recv.read_exact(&mut incoming_len).await?;
+            let len = u64::from_le_bytes(incoming_len);
+
+            let mut buffer = vec![0u8; len as usize];
+            recv.read_exact(&mut buffer).await?;
+            let msg: Protocol = postcard::from_bytes(&buffer)?;
+
+            let is_remote_done = matches!(msg, Protocol::Done);
+
+            // process incominng message
+            if let Protocol::SyncMessage(sync_msg) = msg {
+                let sync_msg = sync::Message::decode(&sync_msg)?;
+                sync.receive_sync_message(&mut sync_state, sync_msg)?;
+            }
+
+            let msg = match sync.generate_sync_message(&mut sync_state) {
+                Some(msg) => Protocol::SyncMessage(msg.encode()),
+                None => Protocol::Done,
+            };
+
+            if !is_local_done {
+                let encoded = postcard::to_stdvec(&msg)?;
+                send.write_all(&(encoded.len() as u64).to_le_bytes())
+                    .await?;
+                send.write_all(&encoded).await?;
+                is_local_done = matches!(msg, Protocol::Done);
+            }
+
+            if is_remote_done && is_local_done {
+                // both sides are done
+                break;
+            }
+        }
+
+        send.finish().await?;
+
+        drop(sync);
+        self.merge_state(commit);
+
+        let commit = self.fork_state();
+        println!("State");
+        let keys: Vec<_> = commit.keys(automerge::ROOT).collect();
+        for key in keys {
+            let (value, _) = commit.get(automerge::ROOT, &key)?.unwrap();
+            println!("{} => {}", key, value);
+        }
+
         Ok(())
     }
 }
 
-impl Deref for Peer {
-    type Target = AutoCommit;
-    fn deref(&self) -> &Self::Target {
-        &self.auto_commit
-    }
-}
-
-impl DerefMut for Peer {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.auto_commit
+impl ProtocolHandler for IrohAutomergeProtocol {
+    fn accept(
+        self: Arc<Self>,
+        conn: iroh::net::endpoint::Connecting,
+    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>> {
+        Box::pin(self.respond_sync(conn))
     }
 }
 
@@ -95,7 +175,7 @@ impl DerefMut for Peer {
 #[command(version, about, long_about = None)]
 struct Cli {
     #[clap(long)]
-    remote_id: Option<inet::NodeId>,
+    remote_id: Option<iroh::net::NodeId>,
     #[clap(long)]
     remote_relay: Option<url::Url>,
 }
@@ -110,117 +190,44 @@ enum Protocol {
 async fn main() -> Result<()> {
     let opts = Cli::parse();
 
-    let mut peer1 = Peer::new().await?;
+    let automerge = IrohAutomergeProtocol::new(AutoCommit::new());
+    let iroh = Node::memory()
+        .build()
+        .await?
+        .accept(
+            IrohAutomergeProtocol::ALPN,
+            Arc::clone(&automerge) as Arc<dyn ProtocolHandler>,
+        )
+        .spawn()
+        .await?;
 
+    let addr = iroh.my_addr().await?;
+
+    println!("Running\nNode Id: {}", addr.node_id,);
+
+    let mut am = automerge.inner.lock().unwrap();
     for i in 0..5 {
-        peer1.put(automerge::ROOT, format!("key-{i}"), format!("value-{i}"))?;
+        am.commit
+            .put(automerge::ROOT, format!("key-{i}"), format!("value-{i}"))?;
     }
-
-    peer1.save();
+    am.commit.save();
+    drop(am);
 
     if let Some(remote_id) = opts.remote_id {
-        let remote_relay = opts.remote_relay.expect("missing derp");
-        let node_addr = inet::NodeAddr::new(remote_id).with_relay_url(remote_relay.into());
-        let conn = peer1.ep.connect(node_addr, ALPN).await?;
-        let (mut send, mut recv) = conn.open_bi().await?;
+        let mut node_addr = iroh::net::NodeAddr::new(remote_id);
 
-        let mut is_local_done = false;
-        loop {
-            let msg = match peer1.generate_sync_message() {
-                Some(msg) => Protocol::SyncMessage(msg.encode()),
-                None => Protocol::Done,
-            };
-
-            if !is_local_done {
-                let encoded = postcard::to_stdvec(&msg)?;
-                send.write_all(&(encoded.len() as u64).to_le_bytes())
-                    .await?;
-                send.write_all(&encoded).await?;
-                is_local_done = matches!(msg, Protocol::Done);
-            }
-
-            let mut incoming_len = [0u8; 8];
-            recv.read_exact(&mut incoming_len).await?;
-            let len = u64::from_le_bytes(incoming_len);
-
-            let mut buffer = vec![0u8; len as usize];
-            recv.read_exact(&mut buffer).await?;
-            let msg: Protocol = postcard::from_bytes(&buffer)?;
-
-            let is_remote_done = matches!(msg, Protocol::Done);
-
-            // process incominng message
-            if let Protocol::SyncMessage(sync_msg) = msg {
-                let sync_msg = sync::Message::decode(&sync_msg)?;
-                peer1.receive_sync_message(sync_msg)?;
-            }
-
-            if is_remote_done && is_local_done {
-                // both sides are done
-                break;
-            }
+        if let Some(remote_relay) = opts.remote_relay {
+            node_addr = node_addr.with_relay_url(remote_relay.into());
         }
 
-        send.finish().await?;
+        let conn = iroh
+            .endpoint()
+            .connect(node_addr, IrohAutomergeProtocol::ALPN)
+            .await?;
+
+        automerge.initiate_sync(conn).await?;
     } else {
-        let mut conn = peer1
-            .ep
-            .accept()
-            .await
-            .ok_or_else(|| anyhow::anyhow!("no connection"))?;
-
-        let alpn = conn.alpn().await?;
-        let conn = conn.await?;
-
-        ensure!(alpn == ALPN, "invalid alpn");
-
-        let (mut send, mut recv) = conn.accept_bi().await?;
-
-        let mut is_local_done = false;
-        loop {
-            let mut incoming_len = [0u8; 8];
-            recv.read_exact(&mut incoming_len).await?;
-            let len = u64::from_le_bytes(incoming_len);
-
-            let mut buffer = vec![0u8; len as usize];
-            recv.read_exact(&mut buffer).await?;
-            let msg: Protocol = postcard::from_bytes(&buffer)?;
-
-            let is_remote_done = matches!(msg, Protocol::Done);
-
-            // process incominng message
-            if let Protocol::SyncMessage(sync_msg) = msg {
-                let sync_msg = sync::Message::decode(&sync_msg)?;
-                peer1.receive_sync_message(sync_msg)?;
-            }
-
-            let msg = match peer1.generate_sync_message() {
-                Some(msg) => Protocol::SyncMessage(msg.encode()),
-                None => Protocol::Done,
-            };
-
-            if !is_local_done {
-                let encoded = postcard::to_stdvec(&msg)?;
-                send.write_all(&(encoded.len() as u64).to_le_bytes())
-                    .await?;
-                send.write_all(&encoded).await?;
-                is_local_done = matches!(msg, Protocol::Done);
-            }
-
-            if is_remote_done && is_local_done {
-                // both sides are done
-                break;
-            }
-        }
-
-        send.finish().await?;
-    }
-
-    println!("State");
-    let keys: Vec<_> = peer1.keys(automerge::ROOT).collect();
-    for key in keys {
-        let (value, _) = peer1.get(automerge::ROOT, &key)?.unwrap();
-        println!("{} => {}", key, value);
+        iroh.cancel_token().cancelled().await;
     }
 
     Ok(())
