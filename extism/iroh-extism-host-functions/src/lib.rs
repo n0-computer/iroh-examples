455e943975b4f6872079f8560849c7578794c70c
“ramfox” <kasey@n0.computer>
2024-12-04 18:53:04 -0500

    fix quic-rpc runtime error

--- extism/iroh-extism-host-functions/src/lib.rs
+++ extism/iroh-extism-host-functions/src/lib.rs
@@ -1,13 +1,14 @@
-use std::path::PathBuf;
-use std::str::FromStr;
+use std::{path::PathBuf, str::FromStr, sync::Arc};
 
 use anyhow::{anyhow, Result};
 use extism::*;
 use futures::stream::StreamExt;
-use iroh::{protocol::Router, Endpoint};
+use iroh::{key::PublicKey, protocol::Router, Endpoint};
 use iroh_blobs::{
-    net_protocol::Blobs,
-    util::{LocalPool, SetTagOption},
+    net_protocol::{Blobs, DownloadMode},
+    rpc::client::blobs::DownloadOptions,
+    store::fs::Store,
+    util::{local_pool::LocalPool, SetTagOption},
 };
 
 const IROH_EXTISM_DATA_DIR: &str = "iroh-extism";
@@ -23,60 +24,93 @@ pub async fn default_iroh_extism_data_root() -> Result<PathBuf> {
     Ok(path.join(IROH_EXTISM_DATA_DIR))
 }
 
-pub async fn create_iroh(path: PathBuf) -> Result<Router> {
-    // create store
-    let store = iroh_blobs::store::fs::Store::load(path)?;
+pub struct Iroh {
+    router: Router,
+    blobs: Arc<Blobs<Store>>,
+    node_id: PublicKey,
+}
 
-    // create an endpoint
-    let endpoint = Endpoint::builder().bind().await?;
-    let addr = endpoint.node_addr().await?;
-    let local_pool = LocalPool::single();
-    // create blobs protocol
-    let downloader = iroh_blobs::downloader::Downloader::new(
-        store.clone(),
-        endpoint.clone(),
-        local_pool.handle().clone(),
-    );
-    let blobs = Arc::new(Blobs::new(
-        store.clone(),
-        local_pool.handle().clone(),
-        Default::default(),
-        downloader.clone(),
-        endpoint.clone(),
-    ));
-    let router = Router::builder(endpoint)
-        .accept(iroh_blobs::ALPN, blobs.clone())
-        .spawn()
-        .await?;
-    Ok(router)
+impl Iroh {
+    pub async fn new(path: PathBuf) -> Result<Iroh> {
+        // create store
+        let store = Store::load(path).await?;
+
+        // create an endpoint
+        let endpoint = Endpoint::builder().bind().await?;
+        let node_id = endpoint.node_id();
+        let local_pool = LocalPool::single();
+
+        // create blobs protocol
+        let downloader = iroh_blobs::downloader::Downloader::new(
+            store.clone(),
+            endpoint.clone(),
+            local_pool.handle().clone(),
+        );
+        let blobs = Arc::new(Blobs::new(
+            store.clone(),
+            local_pool.handle().clone(),
+            Default::default(),
+            downloader.clone(),
+            endpoint.clone(),
+        ));
+        let router = Router::builder(endpoint)
+            .accept(iroh_blobs::ALPN, blobs.clone())
+            .spawn()
+            .await?;
+        Ok(Iroh {
+            router,
+            blobs,
+            node_id,
+        })
+    }
+
+    pub fn node_id(&self) -> PublicKey {
+        self.node_id
+    }
+
+    pub fn blobs(&self) -> Arc<Blobs<Store>> {
+        self.blobs.clone()
+    }
+
+    pub fn router(&self) -> &Router {
+        &self.router
+    }
 }
 
 struct Context {
     rt: tokio::runtime::Handle,
-    iroh: Router,
-    blobs: Blobs,
+    iroh: Iroh,
 }
 
 host_fn!(iroh_blob_get_ticket(user_data: Context; ticket: &str) -> Vec<u8> {
     let ctx = user_data.get()?;
     let ctx = ctx.lock().unwrap();
 
-    let (node_addr, hash, format) = iroh::base::ticket::BlobTicket::from_str(ticket).map_err(|_| anyhow!("invalid ticket"))?.into_parts();
+    let (node_addr, hash, format) = iroh::ticket::BlobTicket::from_str(ticket).map_err(|_| anyhow!("invalid ticket"))?.into_parts();
 
     if format != iroh_blobs::BlobFormat::Raw {
         return Err(anyhow!("can only get raw bytes for now, not HashSequences (collections)"));
     }
-    let client = ctx.iroh.client();
+    let router = ctx.iroh.router();
+    let blobs = ctx.iroh.blobs();
+    println!("blobs");
     let buf = ctx.rt.block_on(async move {
-        let mut stream = client.blobs().download_with_opts(hash, iroh::client::blobs::DownloadOptions {
+        let blobs = blobs.client();
+        let mut stream = blobs.download_with_opts(hash, DownloadOptions {
             format,
             nodes: vec![node_addr],
-            mode: iroh::client::blobs::DownloadMode::Queued,
+            mode: DownloadMode::Queued,
             tag: SetTagOption::Auto,
         }).await?;
+        println!("download");
         while stream.next().await.is_some() {}
+        println!("stream next");
 
-        let buffer = self.blobs.read(hash).await?.read_to_bytes().await?;
+        let buffer = blobs.read(hash).await?.read_to_bytes().await?;
+        println!("read");
+        router.shutdown().await?;
+        println!("shutdown");
+
         anyhow::Ok(buffer.to_vec())
     })?;
 
@@ -86,7 +120,7 @@ host_fn!(iroh_blob_get_ticket(user_data: Context; ticket: &str) -> Vec<u8> {
 pub fn add_all_host_functions(
     rt: tokio::runtime::Handle,
     b: PluginBuilder,
-    iroh: IrohNode,
+    iroh: Iroh,
 ) -> PluginBuilder {
     let ctx = UserData::new(Context { rt, iroh });
 
