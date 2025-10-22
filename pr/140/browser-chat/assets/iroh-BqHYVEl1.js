dd452f88764f145d1c161edb416f8929cc408357
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-10-22 16:03:41 +0000

    deploy: 319c0306199a000110e7c33ba2555215d32294a9

--- pr/140/browser-chat/assets/iroh-BqHYVEl1.js
+++ pr/140/browser-chat/assets/iroh-BqHYVEl1.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-import { l as A, P as V, __tla as __tla_0 } from "./index-B7o4UpDo.js";
+import { l as A, P as V, __tla as __tla_0 } from "./index-CwtrEEGD.js";
 let ne;
 let __tla = Promise.all([
   (() => {
@@ -11,7 +11,7 @@ let __tla = Promise.all([
     }
   })()
 ]).then(async () => {
-  const re = "" + new URL("chat_browser_bg-DHIfy3oU.wasm", import.meta.url).href, oe = async (e = {}, n) => {
+  const re = "" + new URL("chat_browser_bg-5UwXmlja.wasm", import.meta.url).href, oe = async (e = {}, n) => {
     let t;
     if (n.startsWith("data:")) {
       const _ = n.replace(/^data:.*?base64,/, "");
@@ -47,12 +47,12 @@ let __tla = Promise.all([
   });
   D.decode();
   const se = 2146435072;
-  let H = 0;
+  let G = 0;
   function ie(e, n) {
-    return H += n, H >= se && (D = new TextDecoder("utf-8", {
+    return G += n, G >= se && (D = new TextDecoder("utf-8", {
       ignoreBOM: true,
       fatal: true
-    }), D.decode(), H = n), D.decode(E().subarray(e, e + n));
+    }), D.decode(), G = n), D.decode(E().subarray(e, e + n));
   }
   function w(e, n) {
     return e = e >>> 0, ie(e, n);
@@ -200,26 +200,26 @@ ${e.stack}` : _;
     };
     return a.original = o, $.register(a, o, o), a;
   }
-  function de(e, n) {
-    c.__wbindgen_export_5(e, n);
+  function de(e, n, t) {
+    c.__wbindgen_export_5(e, n, s(t));
   }
-  function fe(e, n, t) {
-    c.__wbindgen_export_6(e, n, s(t));
+  function fe(e, n) {
+    c.__wbindgen_export_6(e, n);
   }
   function ue(e, n) {
     c.__wbindgen_export_7(e, n);
   }
-  function ge(e, n) {
-    c.__wbindgen_export_8(e, n);
+  function ge(e, n, t) {
+    c.__wbindgen_export_8(e, n, s(t));
   }
-  function we(e, n, t) {
-    c.__wbindgen_export_9(e, n, s(t));
+  function we(e, n) {
+    c.__wbindgen_export_9(e, n);
   }
-  function le(e, n) {
-    c.__wbindgen_export_10(e, n);
+  function le(e, n, t) {
+    c.__wbindgen_export_10(e, n, s(t));
   }
-  function pe(e, n, t) {
-    c.__wbindgen_export_11(e, n, s(t));
+  function pe(e, n) {
+    c.__wbindgen_export_11(e, n);
   }
   function he(e, n, t, _) {
     c.__wbindgen_export_12(e, n, s(t), s(_));
@@ -513,10 +513,10 @@ ${e.stack}` : _;
       r(e).close();
     }, arguments);
   }
-  function He(e) {
+  function Ge(e) {
     return r(e).code;
   }
-  function Ge(e) {
+  function He(e) {
     return r(e).code;
   }
   function Je(e) {
@@ -777,10 +777,10 @@ ${e.stack}` : _;
   function Vn(e, n, t) {
     Uint8Array.prototype.set.call(T(e, n), r(t));
   }
-  function Hn(e, n) {
+  function Gn(e, n) {
     return r(e).push(r(n));
   }
-  function Gn(e) {
+  function Hn(e) {
     queueMicrotask(r(e));
   }
   function Jn(e) {
@@ -981,10 +981,10 @@ ${e.stack}` : _;
     const t = J(r(n)), _ = y(t, c.__wbindgen_export_0, c.__wbindgen_export_1), o = h;
     d().setInt32(e + 4 * 1, o, true), d().setInt32(e + 4 * 0, _, true);
   }
-  function Ht(e, n) {
+  function Gt(e, n) {
     return r(e) in r(n);
   }
-  function Gt(e) {
+  function Ht(e) {
     return typeof r(e) == "function";
   }
   function Jt(e) {
@@ -1021,19 +1021,19 @@ ${e.stack}` : _;
     return s(n);
   }
   function __(e, n) {
-    const t = C(e, n, 735, fe);
+    const t = C(e, n, 735, le);
     return s(t);
   }
   function r_(e, n) {
-    const t = be(e, n, 1565, ge);
+    const t = be(e, n, 1565, we);
     return s(t);
   }
   function o_(e, n) {
-    const t = C(e, n, 1693, pe);
+    const t = C(e, n, 1693, de);
     return s(t);
   }
   function c_(e, n) {
-    const t = C(e, n, 2755, de);
+    const t = C(e, n, 2755, pe);
     return s(t);
   }
   function s_(e, n) {
@@ -1041,14 +1041,14 @@ ${e.stack}` : _;
     return s(t);
   }
   function i_(e, n) {
-    const t = C(e, n, 1556, le);
+    const t = C(e, n, 1556, fe);
     return s(t);
   }
   function a_(e) {
     return s(e);
   }
   function b_(e, n) {
-    const t = C(e, n, 2763, we);
+    const t = C(e, n, 2763, ge);
     return s(t);
   }
   function d_(e, n) {
@@ -1090,10 +1090,10 @@ ${e.stack}` : _;
       __wbg_abort_d830bf2e9aa6ec5b: Re,
       __wbg_instanceof_Blob_3db67efd3f1b960f: gn,
       __wbg_wasClean_ffb515fbcbcbdd3d: Dt,
-      __wbg_code_177e3bed72688e58: He,
+      __wbg_code_177e3bed72688e58: Ge,
       __wbg_reason_97efd955be6394bd: Qn,
       __wbg_message_5481231e71ccaf7b: mn,
-      __wbg_code_89056d52bf1a8bb0: Ge,
+      __wbg_code_89056d52bf1a8bb0: He,
       __wbg_sethandleevent_504d6c0317f9f4e9: wt,
       __wbg_addEventListener_ae4c27d78f35f886: je,
       __wbg_removeEventListener_7d68951e6508eb3c: et,
@@ -1149,14 +1149,14 @@ ${e.stack}` : _;
       __wbg_randomFillSync_ac0988aba3254290: Xn,
       __wbg_setTimeout_282db1c50d4a7304: ct,
       __wbg_clearTimeout_cbe3107816206ed5: De,
-      __wbg_queueMicrotask_25d0739ac89e8c88: Gn,
+      __wbg_queueMicrotask_25d0739ac89e8c88: Hn,
       __wbg_queueMicrotask_4488407636f5bf24: Jn,
       __wbg_performance_7a3ffd0b17f663ad: $n,
       __wbg_now_2c95c9de01293173: Dn,
       __wbg_next_5b3530e612fde77d: qn,
       __wbg_new_1f3a344cf3123716: vn,
       __wbg_set_90f6c0f7bd8c0415: bt,
-      __wbg_push_330b2eb93e4e1212: Hn,
+      __wbg_push_330b2eb93e4e1212: Gn,
       __wbg_instanceof_ArrayBuffer_67f3012529f6a2dd: un,
       __wbg_new_da9dc54c5db29dfa: Fn,
       __wbg_newnoargs_254190557c45b4ec: En,
@@ -1197,28 +1197,28 @@ ${e.stack}` : _;
       __wbg_Error_e17e777aac105295: ve,
       __wbg_wbindgenisundefined_c4b71d073b92f3c5: Yt,
       __wbg_wbindgenisobject_307a53c6bd97fbf8: Jt,
-      __wbg_wbindgenisfunction_8cee7dce3725ae74: Gt,
+      __wbg_wbindgenisfunction_8cee7dce3725ae74: Ht,
       __wbg_wbindgenisstring_d4fa939789f003b0: Xt,
-      __wbg_wbindgenin_d7a1ee10933d2d55: Ht,
+      __wbg_wbindgenin_d7a1ee10933d2d55: Gt,
       __wbg_wbindgennumberget_f74b4c7525ac05cb: Qt,
       __wbg_wbindgenbooleanget_3fe6f642c7d97746: $t,
       __wbg_wbindgenstringget_0f16a6ddddef376f: Zt,
       __wbg_wbindgenthrow_451ec1a8469d7eb6: e_,
       __wbg_wbindgencbdrop_eb10308566512b88: Pt,
       __wbg_wbindgenjsvallooseeq_9bec8c9be826bed1: Kt,
-      __wbindgen_cast_c2c989a27a0562df: c_,
-      __wbindgen_cast_4d0daddcc392ccc2: __,
+      __wbindgen_cast_cb9088102bce6b30: s_,
+      __wbindgen_cast_84c6047cf3a3a26f: o_,
+      __wbindgen_cast_d1cc5ed77062331f: i_,
       __wbindgen_cast_fc4b8e9259f7cb57: d_,
+      __wbindgen_cast_d6cd19b81560fd6e: a_,
+      __wbindgen_cast_e1c34903082403c1: b_,
       __wbindgen_cast_2241b6af4c4b2941: n_,
       __wbindgen_cast_835a3fca9ef01735: r_,
-      __wbindgen_cast_e1c34903082403c1: b_,
-      __wbindgen_cast_cb9088102bce6b30: s_,
-      __wbindgen_cast_d1cc5ed77062331f: i_,
-      __wbindgen_cast_84c6047cf3a3a26f: o_,
-      __wbindgen_cast_d6cd19b81560fd6e: a_,
-      __wbindgen_cast_4625c577ab2ec9ee: t_
+      __wbindgen_cast_4d0daddcc392ccc2: __,
+      __wbindgen_cast_4625c577ab2ec9ee: t_,
+      __wbindgen_cast_c2c989a27a0562df: c_
     }
-  }, re), g_ = i.memory, w_ = i.start, l_ = i.__wbg_chatnode_free, p_ = i.chatnode_spawn, h_ = i.chatnode_endpoint_id, y_ = i.chatnode_create, m_ = i.chatnode_join, x_ = i.__wbg_channel_free, S_ = i.channel_sender, v_ = i.channel_receiver, k_ = i.channel_ticket, I_ = i.channel_id, R_ = i.channel_neighbors, j_ = i.__wbg_channelsender_free, F_ = i.channelsender_broadcast, T_ = i.channelsender_set_nickame, C_ = i.__wbg_intounderlyingbytesource_free, A_ = i.intounderlyingbytesource_type, E_ = i.intounderlyingbytesource_autoAllocateChunkSize, O_ = i.intounderlyingbytesource_start, L_ = i.intounderlyingbytesource_pull, M_ = i.intounderlyingbytesource_cancel, N_ = i.__wbg_intounderlyingsource_free, U_ = i.intounderlyingsource_pull, q_ = i.intounderlyingsource_cancel, B_ = i.__wbg_intounderlyingsink_free, z_ = i.intounderlyingsink_write, W_ = i.intounderlyingsink_close, D_ = i.intounderlyingsink_abort, $_ = i.ring_core_0_17_14__bn_mul_mont, P_ = i.__wbindgen_export_0, V_ = i.__wbindgen_export_1, H_ = i.__wbindgen_export_2, G_ = i.__wbindgen_export_3, J_ = i.__wbindgen_export_4, X_ = i.__wbindgen_add_to_stack_pointer, Y_ = i.__wbindgen_export_5, K_ = i.__wbindgen_export_6, Q_ = i.__wbindgen_export_7, Z_ = i.__wbindgen_export_8, er = i.__wbindgen_export_9, nr = i.__wbindgen_export_10, tr = i.__wbindgen_export_11, _r = i.__wbindgen_export_12, ee = i.__wbindgen_start, rr = Object.freeze(Object.defineProperty({
+  }, re), g_ = i.memory, w_ = i.start, l_ = i.__wbg_chatnode_free, p_ = i.chatnode_spawn, h_ = i.chatnode_endpoint_id, y_ = i.chatnode_create, m_ = i.chatnode_join, x_ = i.__wbg_channel_free, S_ = i.channel_sender, v_ = i.channel_receiver, k_ = i.channel_ticket, I_ = i.channel_id, R_ = i.channel_neighbors, j_ = i.__wbg_channelsender_free, F_ = i.channelsender_broadcast, T_ = i.channelsender_set_nickame, C_ = i.__wbg_intounderlyingbytesource_free, A_ = i.intounderlyingbytesource_type, E_ = i.intounderlyingbytesource_autoAllocateChunkSize, O_ = i.intounderlyingbytesource_start, L_ = i.intounderlyingbytesource_pull, M_ = i.intounderlyingbytesource_cancel, N_ = i.__wbg_intounderlyingsource_free, U_ = i.intounderlyingsource_pull, q_ = i.intounderlyingsource_cancel, B_ = i.__wbg_intounderlyingsink_free, z_ = i.intounderlyingsink_write, W_ = i.intounderlyingsink_close, D_ = i.intounderlyingsink_abort, $_ = i.ring_core_0_17_14__bn_mul_mont, P_ = i.__wbindgen_export_0, V_ = i.__wbindgen_export_1, G_ = i.__wbindgen_export_2, H_ = i.__wbindgen_export_3, J_ = i.__wbindgen_export_4, X_ = i.__wbindgen_add_to_stack_pointer, Y_ = i.__wbindgen_export_5, K_ = i.__wbindgen_export_6, Q_ = i.__wbindgen_export_7, Z_ = i.__wbindgen_export_8, er = i.__wbindgen_export_9, nr = i.__wbindgen_export_10, tr = i.__wbindgen_export_11, _r = i.__wbindgen_export_12, ee = i.__wbindgen_start, rr = Object.freeze(Object.defineProperty({
     __proto__: null,
     __wbg_channel_free: x_,
     __wbg_channelsender_free: j_,
@@ -1232,8 +1232,8 @@ ${e.stack}` : _;
     __wbindgen_export_10: nr,
     __wbindgen_export_11: tr,
     __wbindgen_export_12: _r,
-    __wbindgen_export_2: H_,
-    __wbindgen_export_3: G_,
+    __wbindgen_export_2: G_,
+    __wbindgen_export_3: H_,
     __wbindgen_export_4: J_,
     __wbindgen_export_5: Y_,
     __wbindgen_export_6: K_,
@@ -1337,7 +1337,7 @@ ${e.stack}` : _;
               content: l.text
             };
             g.messages.push(z);
-            const _e = G(g, z);
+            const _e = H(g, z);
             for (const P of g.subscribers) P(_e);
             for (const P of g.peerSubscribers) P();
           } else if (l.type === "presence") {
@@ -1411,7 +1411,7 @@ ${e.stack}` : _;
         content: t
       };
       _.messages.push(a);
-      const b = G(_, a);
+      const b = H(_, a);
       for (const u of _.subscribers) u(b);
     }
     setNickname(n, t) {
@@ -1423,7 +1423,7 @@ ${e.stack}` : _;
     getMessages(n) {
       const t = this.channels.get(n);
       if (!t) throw new Error("Channel not found");
-      return t.messages.map((o) => G(t, o));
+      return t.messages.map((o) => H(t, o));
     }
     getPeers(n) {
       const t = this.channels.get(n);
@@ -1456,7 +1456,7 @@ ${e.stack}` : _;
     const t = e.peers.get(n);
     return t && t.name ? t.name : n.substring(0, 8);
   }
-  function G(e, n) {
+  function H(e, n) {
     return {
       ...n,
       nickname: or(e, n.sender)
--- pr/140/browser-echo/wasm/browser_echo.d.ts
+++ pr/140/browser-echo/wasm/browser_echo.d.ts
@@ -72,12 +72,12 @@ export interface InitOutput {
   readonly __wbindgen_export_3: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_4: WebAssembly.Table;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly __wbindgen_export_5: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_5: (a: number, b: number) => void;
   readonly __wbindgen_export_6: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_export_7: (a: number, b: number) => void;
-  readonly __wbindgen_export_8: (a: number, b: number) => void;
+  readonly __wbindgen_export_7: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_8: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_9: (a: number, b: number) => void;
-  readonly __wbindgen_export_10: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_10: (a: number, b: number) => void;
   readonly __wbindgen_export_11: (a: number, b: number, c: number, d: number) => void;
   readonly __wbindgen_start: () => void;
 }
