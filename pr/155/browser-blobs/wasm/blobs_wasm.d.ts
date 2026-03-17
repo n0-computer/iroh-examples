796b2f83d5a49b29a306e05326877ec0446b4756
rklaehn <rklaehn@users.noreply.github.com>
2026-03-17 12:21:24 +0000

    deploy: 0a163ea45524dba7d2539ae947a7d5d5dea08192

---
+++ pr/155/browser-blobs/wasm/blobs_wasm.d.ts
@@ -0,0 +1,125 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * The `ReadableStreamType` enum.
+ *
+ * *This API requires the following crate features to be activated: `ReadableStreamType`*
+ */
+
+type ReadableStreamType = "bytes";
+
+export class BlobsNode {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    complete_size(hash: string): Promise<bigint>;
+    download(ticket: string): Promise<string>;
+    endpoint_id(): string;
+    get(hash: string): Promise<Uint8Array>;
+    import(data: Uint8Array): Promise<string>;
+    static spawn(): Promise<BlobsNode>;
+}
+
+export class IntoUnderlyingByteSource {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    cancel(): void;
+    pull(controller: ReadableByteStreamController): Promise<any>;
+    start(controller: ReadableByteStreamController): void;
+    readonly autoAllocateChunkSize: number;
+    readonly type: ReadableStreamType;
+}
+
+export class IntoUnderlyingSink {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    abort(reason: any): Promise<any>;
+    close(): Promise<any>;
+    write(chunk: any): Promise<any>;
+}
+
+export class IntoUnderlyingSource {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    cancel(): void;
+    pull(controller: ReadableStreamDefaultController): Promise<any>;
+}
+
+export function start(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_blobsnode_free: (a: number, b: number) => void;
+    readonly blobsnode_complete_size: (a: number, b: number, c: number) => any;
+    readonly blobsnode_download: (a: number, b: number, c: number) => any;
+    readonly blobsnode_endpoint_id: (a: number) => [number, number];
+    readonly blobsnode_get: (a: number, b: number, c: number) => any;
+    readonly blobsnode_import: (a: number, b: any) => any;
+    readonly blobsnode_spawn: () => any;
+    readonly start: () => void;
+    readonly __wbg_intounderlyingsink_free: (a: number, b: number) => void;
+    readonly intounderlyingsink_abort: (a: number, b: any) => any;
+    readonly intounderlyingsink_close: (a: number) => any;
+    readonly intounderlyingsink_write: (a: number, b: any) => any;
+    readonly __wbg_intounderlyingbytesource_free: (a: number, b: number) => void;
+    readonly intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+    readonly intounderlyingbytesource_cancel: (a: number) => void;
+    readonly intounderlyingbytesource_pull: (a: number, b: any) => any;
+    readonly intounderlyingbytesource_start: (a: number, b: any) => void;
+    readonly intounderlyingbytesource_type: (a: number) => number;
+    readonly __wbg_intounderlyingsource_free: (a: number, b: number) => void;
+    readonly intounderlyingsource_cancel: (a: number) => void;
+    readonly intounderlyingsource_pull: (a: number, b: any) => any;
+    readonly ring_core_0_17_14__bn_mul_mont: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+    readonly wasm_bindgen__closure__destroy__hd3ac853b05f47023: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__ha2c141d9ee6b88f6: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__ha5eeef7d78c2e2f7: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h1e4b93d1f1419bba: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h276bbc7daad97315: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h32c21b93cbd3feca: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__hec4b29efce2a3272: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h8506664ff5ef0a56: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h19b4abff8bf432fb: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h2d58a232ff60a071: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hd79d89080bd20057: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h03f589a7dd5aaf90: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0d77d15a417cbf9e: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hf48cc941c723898b: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hc3a18e22b51075fe: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_drop_slice: (a: number, b: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
